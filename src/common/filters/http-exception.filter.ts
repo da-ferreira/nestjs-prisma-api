c762566dadc979db00d427478d7f2088778a5e6f
da-ferreira <ferreiradavid361@gmail.com>
2022-10-12 18:32:23 -0300

    feat: aplicando um exception filter de forma global

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter<T extends HttpException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const request = context.getRequest<Request>();
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+
+    const error =
+      typeof response === 'string'
+        ? { message: exceptionResponse }
+        : (exceptionResponse as object);
+
+    response.status(status).json({
+      ...error,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
