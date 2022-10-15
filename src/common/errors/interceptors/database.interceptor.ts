be01545c8ab7159de612a833437d9755c57d9fb5
da-ferreira <ferreiradavid361@gmail.com>
2022-10-15 16:47:06 -0300

    database interceptor e validações de erro do prisma

--- src/common/errors/interceptors/database.interceptor.ts
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -3,8 +3,11 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  BadRequestException,
 } from '@nestjs/common';
 import { catchError, Observable } from 'rxjs';
+import { DatabaseError } from '../types/DatabaseError';
+import { handleDatabaseErrors } from '../utils/handle-database-errors.util';
 import { isPrismaError } from '../utils/is-prisma-error.util';
 
 @Injectable()
@@ -13,7 +16,10 @@ export class DatabaseInterceptor implements NestInterceptor {
     return next.handle().pipe(
       catchError(error => {
         if (isPrismaError(error)) {
-          //
+          error = handleDatabaseErrors(error);
+        }
+        if (error instanceof DatabaseError) {
+          throw new BadRequestException(error.message);
         } else {
           throw error;
         }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 import { NotFoundInterceptor } from './common/errors/interceptors/notFound.interceptor';
 import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
@@ -19,9 +20,10 @@ async function bootstrap() {
   // app.useGlobalFilters(new HttpExceptionFilter());
 
   // A aplicação vai verificar todas as requisições que chegarem
+  app.useGlobalInterceptors(new ConflictInterceptor());
+  app.useGlobalInterceptors(new DatabaseInterceptor());
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
   app.useGlobalInterceptors(new NotFoundInterceptor());
-  app.useGlobalInterceptors(new DatabaseInterceptor());
 
   await app.listen(process.env.PORT || 3000);
 }
